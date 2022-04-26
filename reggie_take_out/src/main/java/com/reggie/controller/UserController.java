7a70bfafd05d14c84a17670781052d25f0dd5cbc
Case <bai2953752874@163.com>
2022-04-26 17:59:53 +0800

    4.26 提交：     新增：前台用户登录(发送验证码)

--- reggie_take_out/src/main/java/com/reggie/controller/UserController.java
+++ reggie_take_out/src/main/java/com/reggie/controller/UserController.java
@@ -1,14 +1,19 @@
 package com.reggie.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.reggie.common.R;
 import com.reggie.entity.User;
 import com.reggie.service.UserService;
+import com.reggie.sms.Sample;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpSession;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/user")
@@ -19,12 +24,55 @@ public class UserController {
     private UserService userService;
 
     /**
-     * 用户登录
+     * 发送短信
+     *
      * @param user
      * @return
      */
     @PostMapping("/sendMsg")
-    public R<String> sendMsg(@RequestBody User user) {
-        return null;
+    public R<String> sendMsg(@RequestBody User user) throws Exception {
+        log.info("接收到请求，即将发送验证码");
+        Sample.sms();
+        return R.success("验证码已发送");
     }
+
+    /**
+     * 登录
+     *
+     * @param map
+     * @param httpSession
+     * @return
+     */
+    @PostMapping("/login")
+    public R<User> login(@RequestBody Map map, HttpSession httpSession) {
+        //获取手机号，验证码
+        String phone = map.get("phone").toString();
+        String code = map.get("code").toString();
+        //获取验证码，test
+        String attribute = "1234";
+
+        //验证码比对
+        if (attribute.equals(code)) {
+            LambdaQueryWrapper<User> queryWrapper = new LambdaQueryWrapper<>();
+            queryWrapper.eq(User::getPhone, phone);
+            User user = userService.getOne(queryWrapper);
+
+            if (user == null) {
+                //新用户，注册
+                user = new User();
+                user.setPhone(phone);
+                user.setStatus(1);
+                userService.save(user);
+
+            }
+
+            httpSession.setAttribute("user", user.getId());
+            return R.success(user);
+
+        }
+
+        return R.error("登录失败");
+    }
+
+
 }
--- reggie_take_out/src/main/java/com/reggie/sms/Sample.java
+++ reggie_take_out/src/main/java/com/reggie/sms/Sample.java
@@ -28,7 +28,7 @@ public class Sample {
     }
 
     public static void sms() throws Exception {
-        com.aliyun.dysmsapi20170525.Client client = Sample.createClient("test", "test");
+        com.aliyun.dysmsapi20170525.Client client = Sample.createClient("LTAI5tSFWm9vq2CCHWU2VrT9", "qyyCvzYDCJoyMVmZZYXW9Ahg7w5IFx");
         SendSmsRequest sendSmsRequest = new SendSmsRequest()
                 .setSignName("阿里云短信测试")
                 .setTemplateCode("SMS_154950909")
